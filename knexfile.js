11941bdf801826eca36752d47821ef7362d86048
Aldo Mercado <amercado1014@gmail>
2018-05-02 20:38:21 -0600

    Add seed file

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,13 @@
 
 module.exports = {
   development: {
-    client: "pg",
-    connection: "postgres://localhost/palettes",
+    client: 'pg',
+    connection: 'postgres://localhost/palettes',
     migrations: {
-      directory: "./db/migrations"
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
