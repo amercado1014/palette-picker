7c3e6320e6beb8387a53602ea89d5bfbd714329b
Aldo Mercado <amercado1014@gmail>
2018-05-04 10:34:21 -0600

    Add heroku

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,33 @@
 module.exports = {
   development: {
-    client: "pg",
-    connection: "postgres://localhost/palettes",
+    client: 'pg',
+    connection: 'postgres://localhost/palettes',
     migrations: {
-      directory: "./db/migrations"
+      directory: './db/migrations'
     },
     seeds: {
-      directory: "./db/seeds/dev"
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   },
   test: {
-    client: "pg",
-    connection: "postgres://localhost/palettes_test",
+    client: 'pg',
+    connection: 'postgres://localhost/palettes_test',
     migrations: {
-      directory: "./db/migrations"
+      directory: './db/migrations'
     },
     seeds: {
-      directory: "./db/seeds/test"
+      directory: './db/seeds/test'
     },
     useNullAsDefault: true
-  }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+  },
+  useNullAsDefault: true
+}
+
 };
