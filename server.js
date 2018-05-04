04b0febaf931890371d67ffe70cbccd9e93e8cde
Aldo Mercado <amercado1014@gmail>
2018-05-03 20:20:05 -0600

    Add get tests

--- server.js
+++ server.js
@@ -82,6 +82,6 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
-module.exports = app;
+module.exports = { app, database };
 
 
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,19 +1,80 @@
 const chai = require('chai');
 const should = chai.should();
-
-const server = require('../server');
-
+const { app, database } = require('../server');
 const chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
 
-describe('Testing endpoints', () => {
- it('GET all projects', (done) => {
-  chai.request(server)
-    .get('/api/v1/projects')
-    .end((err, response) => {
-      response.should.be.json;
-      response.should.have.status(200);
-      done();
-    })
- });
+describe('API Routes', () => {
+  beforeEach(done => {
+    database.migrate.rollback()
+      .then(() => {
+        database.migrate.latest()
+          .then(() => {
+            return database.seed.run()
+              .then(() => {
+                done();
+              });
+          });
+      });
+  });
+
+  it('GET projects should return all the projects', done => {
+    chai.request(app)
+      .get('/api/v1/projects')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.an('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('project_name');
+        response.body[0].project_name.should.equal('Project1');
+        done();
+      })
+  });
+
+  it('GET palettes should return all of the palettes', done => {
+    chai.request(app)
+      .get('/api/v1/palettes')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.an('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('palette_name');
+        response.body[0].palette_name.should.equal('Palette1');
+        response.body[0].should.have.property('colors_array');
+        response.body[0].colors_array.should.be.an('array');
+        response.body[0].colors_array.length.should.equal(5);
+        done();
+      })
+  });
+
+  it('GET palette by id should return a single palette', done => {
+    const paletteId = 1;
+
+    chai.request(app)
+      .get(`/api/v1/palettes/${paletteId}`)
+      .send({
+        id: paletteId
+      })
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.an('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('palette_name');
+        response.body[0].palette_name.should.equal('Palette1');
+        response.body[0].should.have.property('colors_array');
+        response.body[0].colors_array.should.be.an('array');
+        response.body[0].colors_array.length.should.equal(5);
+        done();
+      })
+  });
 });
